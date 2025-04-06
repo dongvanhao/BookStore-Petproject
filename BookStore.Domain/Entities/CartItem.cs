0e869d32fb95b796391c66be963853e8d88213cc
DongHao <dongvanhao12345@gmail.com>
2025-04-07 02:49:35 +0700

    Add Domain

---
+++ BookStore.Domain/Entities/CartItem.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookStore.Domain.Entities
+{
+    public class CartItem
+    {
+        [Key] // Khóa chính
+        public int Id { get; set; }
+
+        [Required]
+        [ForeignKey("Cart")] // FK đến giỏ hàng
+        public int CartId { get; set; }
+
+        public Cart Cart { get; set; }
+
+        [Required]
+        [ForeignKey("Book")] // FK đến sách
+        public int BookId { get; set; }
+
+        public Book Book { get; set; }
+
+        [Required] // Số lượng sách trong giỏ
+        public int Quantity { get; set; }
+    }
+}
